45967c94704d88024a42554f9e395343e1172bd7
Lika Sizih <likasizih@mail.ru>
2022-09-21 22:00:29 +0300

    did Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите три любых числа");
+int number_a = int.Parse(Console.ReadLine());
+int number_b = int.Parse(Console.ReadLine());
+int number_c = int.Parse(Console.ReadLine());
+int max = number_a;
+
+if (number_b > max)
+{
+    max = number_b;
+}
+if (number_c > max)
+{
+    max = number_c;
+}
+Console.WriteLine("Max = " + max);
