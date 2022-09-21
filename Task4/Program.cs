415d5106c4a6362811ce302719b780457429090b
Lika Sizih <likasizih@mail.ru>
2022-09-21 22:19:44 +0300

    did Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int x = 1;
+
+System.Console.WriteLine("Все чётные числа от 1 до " + number);
+while (x < number)
+{
+    if(x % 2 == 0)
+    {
+        Console.WriteLine(x);
+    }
+    x++;
+}
