f3f9ec3998c71fb86b5950d304cb3adf4fbbe1fc
Lika Sizih <likasizih@mail.ru>
2022-09-21 21:43:49 +0300

    did Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число а");
+int number_a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b");
+int number_b = int.Parse(Console.ReadLine());
+
+if (number_a > number_b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(number_a);
+    Console.Write("min = ");
+    Console.WriteLine(number_b);
+
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(number_b);
+    Console.Write("min = ");
+    Console.WriteLine(number_a);
+}
